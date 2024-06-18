cfac5dd9e45b5824e828c82bd120ea20791a029c
jeflorentino <jessica.florentino@cloudwalk.io>
2024-06-18 06:08:31 -0300

    Add validations to Phonecontact model

--- app/models/phonecontact.rb
+++ app/models/phonecontact.rb
@@ -1,2 +1,5 @@
 class Phonecontact < ApplicationRecord
+    validates :name, presence: true
+    validates :phone_number, presence: true, uniqueness: true
+    validates :notes, length: { maximum: 500 }, allow_blank: true
 end
