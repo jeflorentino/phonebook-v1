439b7aaaec83fac258db2d69bc57a0ef4ee2ebe8
jeflorentino <jessica.florentino@cloudwalk.io>
2024-07-06 09:44:40 -0300

    Add Jest configuration and setup for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Yellowzim/i);
   expect(linkElement).toBeInTheDocument();
 });
